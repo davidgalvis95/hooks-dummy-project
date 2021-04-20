c39da6361cdf16f217a4f4f95531cd26338aa6a1
David Galvis <ivan.galvis@project44.com>
2021-04-19 21:21:23 -0500

    multiple states

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -6,7 +6,8 @@ import './IngredientForm.css';
 const IngredientForm = React.memo(props => {
 
   //due that the useState is formed of 2 elements in an array we can take advantage of array destructuring to assign the properties and make it more verbose
-  const [inputState, setInputState] = useState({title: '', amount: ''});
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
@@ -21,31 +22,18 @@ const IngredientForm = React.memo(props => {
             <label htmlFor="title">Name</label>
             {/*We had to update the whole object in order to update the state*/}
             <input
-                type="text" id="title" value={inputState.title}
-                onChange={event => {
-                  const newTitle = event.target.value;
-                  //This is the way to ensure that we are updating te state based on the last available state, in the old one we could not ensure that we get the last available
-                  setInputState(prevInputState => ({
-                    title: newTitle,
-                    amount: prevInputState.amount
-                  }))
-                }}/>
+                type="text" id="title" value={enteredTitle}
+                onChange={event => setEnteredTitle(event.target.value)}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             {/*here we just get the 0 because use state result is an array of 2 position where the first one is the actual updated state, and the second
             is the function that updates the state*/}
+            {/*When working with multiple states, due that it's pretty cumbersome to update the whole object if the case is the one of a pretty large object
+             it's better to separate the state into single properties*/}
             <input
-                type="number" id="amount" value={inputState.amount}
-                onChange={event =>{
-                    //Since this is a closure, the event input variable is not reachable by the second key stroke it dies and running time,
-                  // when this is passed to the function parameters
-                    const newAmount = event.target.value;
-                    setInputState(prevInputState => ({
-                      title: prevInputState.title,
-                      amount: newAmount
-                    }))
-                }}/>
+                type="number" id="amount" value={enteredAmount}
+                onChange={event => setEnteredAmount(event.target.value)}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
