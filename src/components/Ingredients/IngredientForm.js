0c0d00c123c7956c9aa0f7176df11e0591420518
David Galvis <ivan.galvis@project44.com>
2021-04-19 21:02:14 -0500

    using array destructuring for useState explicity

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -5,7 +5,8 @@ import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
 
-  const inputState = useState({title: '', amount: ''});
+  //due that the useState is formed of 2 elements in an array we can take advantage of array destructuring to assign the properties and make it more verbose
+  const [inputState, setInputState] = useState({title: '', amount: ''});
 
   const submitHandler = event => {
     event.preventDefault();
@@ -20,11 +21,11 @@ const IngredientForm = React.memo(props => {
             <label htmlFor="title">Name</label>
             {/*We had to update the whole object in order to update the state*/}
             <input
-                type="text" id="title" value={inputState[0].title}
+                type="text" id="title" value={inputState.title}
                 onChange={event => {
                   const newTitle = event.target.value;
                   //This is the way to ensure that we are updating te state based on the last available state, in the old one we could not ensure that we get the last available
-                  inputState[1](prevInputState => ({
+                  setInputState(prevInputState => ({
                     title: newTitle,
                     amount: prevInputState.amount
                   }))
@@ -35,12 +36,12 @@ const IngredientForm = React.memo(props => {
             {/*here we just get the 0 because use state result is an array of 2 position where the first one is the actual updated state, and the second
             is the function that updates the state*/}
             <input
-                type="number" id="amount" value={inputState[0].amount}
+                type="number" id="amount" value={inputState.amount}
                 onChange={event =>{
                     //Since this is a closure, the event input variable is not reachable by the second key stroke it dies and running time,
                   // when this is passed to the function parameters
                     const newAmount = event.target.value;
-                    inputState[1](prevInputState => ({
+                    setInputState(prevInputState => ({
                       title: prevInputState.title,
                       amount: newAmount
                     }))
