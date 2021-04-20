c9987ffeb4bd810d816543a615cf307aa1fdc543
David Galvis <ivan.galvis@project44.com>
2021-04-19 20:38:43 -0500

    Getting started with useState

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+
+  const inputState = useState({title: '', amount: ''});
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +18,16 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            {/*This approach of updating the state does not work well cause this is not like the state of the class based components, where
+            the changed state is merged into the old state, here it is not merged, and if we are working with objects as in this case, we have to update the whole object
+            instead of a single property of that object*/}
+            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            {/*here we just get the 0 because use state result is an array of 2 position where the first one is the actual updated state, and the second
+            is the function that updates the state*/}
+            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
