891864d8c4c169c6ff2cc07315bdb5a33412de37
David Galvis <ivan.galvis@project44.com>
2021-04-19 20:56:04 -0500

    mode on useState and state updating

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -18,16 +18,33 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            {/*This approach of updating the state does not work well cause this is not like the state of the class based components, where
-            the changed state is merged into the old state, here it is not merged, and if we are working with objects as in this case, we have to update the whole object
-            instead of a single property of that object*/}
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})}/>
+            {/*We had to update the whole object in order to update the state*/}
+            <input
+                type="text" id="title" value={inputState[0].title}
+                onChange={event => {
+                  const newTitle = event.target.value;
+                  //This is the way to ensure that we are updating te state based on the last available state, in the old one we could not ensure that we get the last available
+                  inputState[1](prevInputState => ({
+                    title: newTitle,
+                    amount: prevInputState.amount
+                  }))
+                }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             {/*here we just get the 0 because use state result is an array of 2 position where the first one is the actual updated state, and the second
             is the function that updates the state*/}
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})}/>
+            <input
+                type="number" id="amount" value={inputState[0].amount}
+                onChange={event =>{
+                    //Since this is a closure, the event input variable is not reachable by the second key stroke it dies and running time,
+                  // when this is passed to the function parameters
+                    const newAmount = event.target.value;
+                    inputState[1](prevInputState => ({
+                      title: prevInputState.title,
+                      amount: newAmount
+                    }))
+                }}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
