79503730e2cf18db034460df9513d4a449c031cb
David Galvis <ivan.galvis@project44.com>
2021-04-20 20:15:36 -0500

    sending HTTP requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -9,11 +9,21 @@ const Ingredients = () => {
     const [ingredients, setIngredients] = useState([]);
 
     const addIngredientsHandler = ingredient =>{
-        setIngredients(prevIngredientsState => [...prevIngredientsState, { id:
-                Math.random().toString(),
-            ingredient
-        }
-        ]);
+        //Whenever this handler is executed we save the new ingredient in the FIREBASE database and display it there with the previous ingredients
+        fetch('https://react-hooks-update-6eb9b-default-rtdb.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient),
+            headers: {'Content-Type': 'application/json'}
+        }).then(response => {
+            return response.json();
+        }).then(responseData => {
+            setIngredients(prevIngredientsState => [...prevIngredientsState, {
+                //this id is set accordingly to the one provided in FIREBASE, instead of passing a dummy one
+                id: responseData.name,
+                ...ingredient
+            }
+            ]);
+        })
     }
 
     return (
