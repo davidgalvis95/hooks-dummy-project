a3f9681c49654e5eca10be488a3f59e3dc21d96e
David Galvis <ivan.galvis@project44.com>
2021-04-20 20:49:37 -0500

    useEffect and loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from "./IngredientForm";
 import Search from './Search';
@@ -7,6 +7,28 @@ import IngredientList from "./IngredientList";
 const Ingredients = () => {
 
     const [ingredients, setIngredients] = useState([]);
+
+    //The useEffect when without the [] in the end is like a componentDidUpdate, will run after every component update or re render
+    //with [] as a dependency it acts as a componentDidMount, it runs only once after the first render
+    useEffect(() => {
+        fetch('https://react-hooks-update-6eb9b-default-rtdb.firebaseio.com/ingredients.json')
+            .then(response => response.json())
+            .then(responseData => {
+                const loadedIngredients = [];
+                for(const key in responseData) {
+                    loadedIngredients.push({
+                        id: key,
+                        title: responseData[key].title,
+                        amount: responseData[key].amount
+                    })
+                }
+                //this will cause an infinite loop cause we are updating the state and the component is getting re-rendered, and then again
+                // when the component is re-rendered we update the state and so on
+                // setIngredients(loadedIngredients);
+            });
+    }, []);
+
+
 
     const addIngredientsHandler = ingredient =>{
         //Whenever this handler is executed we save the new ingredient in the FIREBASE database and display it there with the previous ingredients
