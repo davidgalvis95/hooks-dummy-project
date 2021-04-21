796dfa315f15ddd5a27274654d1dacb1ed054fea
David Galvis <ivan.galvis@project44.com>
2021-04-20 19:56:40 -0500

    Passing state data across components

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import IngredientForm from './IngredientForm';
+import IngredientForm from "./IngredientForm";
 import Search from './Search';
+import IngredientList from "./IngredientList";
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+const Ingredients = () => {
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
+    const [ingredients, setIngredients] = useState([]);
+
+    const addIngredientsHandler = ingredient =>{
+        setIngredients(prevIngredientsState => [...prevIngredientsState, { id:
+                Math.random().toString(),
+            ingredient
+        }
+        ]);
+    }
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientsHandler}/>
+
+            <section>
+                <Search/>
+                <IngredientList ingredients={ingredients} onRemoveItem={() => {}}/>
+            </section>
+        </div>
+    );
 }
 
 export default Ingredients;
