24b31e06573ee77e1471a35f280dfc78ded384e1
David Galvis <ivan.galvis@project44.com>
2021-04-29 22:53:51 -0500

    Refs and useRef()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,25 +8,26 @@ const Ingredients = () => {
 
     const [ingredients, setIngredients] = useState([]);
 
+    //this useEffect is doing the same than in search but with all the ingredients, something done already in search useEffect
     //The useEffect when without the [] in the end is like a componentDidUpdate, will run after every component update or re render
     //with [] as a dependency it acts as a componentDidMount, it runs only once after the first render
-    useEffect(() => {
-        fetch('https://react-hooks-update-6eb9b-default-rtdb.firebaseio.com/ingredients.json')
-            .then(response => response.json())
-            .then(responseData => {
-                const loadedIngredients = [];
-                for(const key in responseData) {
-                    loadedIngredients.push({
-                        id: key,
-                        title: responseData[key].title,
-                        amount: responseData[key].amount
-                    })
-                }
-                //this will cause an infinite loop cause we are updating the state and the component is getting re-rendered, and then again
-                // when the component is re-rendered we update the state and so on
-                setIngredients(loadedIngredients);
-            });
-    }, []);
+    // useEffect(() => {
+    //     fetch('https://react-hooks-update-6eb9b-default-rtdb.firebaseio.com/ingredients.json')
+    //         .then(response => response.json())
+    //         .then(responseData => {
+    //             const loadedIngredients = [];
+    //             for(const key in responseData) {
+    //                 loadedIngredients.push({
+    //                     id: key,
+    //                     title: responseData[key].title,
+    //                     amount: responseData[key].amount
+    //                 })
+    //             }
+    //             //this will cause an infinite loop cause we are updating the state and the component is getting re-rendered, and then again
+    //             // when the component is re-rendered we update the state and so on
+    //             setIngredients(loadedIngredients);
+    //         });
+    // }, []);
 
 
     useEffect(() => {
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -8,27 +8,37 @@ const Search = React.memo(props => {
     //this is object destructuring to obtain certain object/property from the parent object
     const {onLoadIngredients} = props;
     const [enteredFilter, setEnteredFilter] = useState('');
+    //useRef gives us the current value of any property we link to
+    const inputSearchRef = useRef();
 
     //This will only be executed when the enteredFilter changes
     useEffect(() => {
-        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch('https://react-hooks-update-6eb9b-default-rtdb.firebaseio.com/ingredients.json' + query)
-            .then(response => response.json())
-            .then(responseData => {
-                const loadedIngredients = [];
-                for(const key in responseData) {
-                    loadedIngredients.push({
-                        id: key,
-                        title: responseData[key].title,
-                        amount: responseData[key].amount
-                    })
-                }
-                //we had a warning of including the props as a dependency because here we are relying on props
-                //when destructuring since this is a dependency too, we can add that in the deps of useEffect and invoke without props keyword
-                onLoadIngredients(loadedIngredients);
-                //..
-            });
-    }, [enteredFilter, onLoadIngredients])
+        setTimeout( () => {
+            //For closures, the enteredFilter variable is locked in when that one is called, so it is not the current filter, but the one that was taken
+            //500 ms before when the setTimeout closure started
+            //so here i need to know if the current value of that filter, is the same to what was entered 500 ms ago, so that we know that is the value the user wants to go with
+            if(enteredFilter === inputSearchRef.current.value){
+                const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+                fetch('https://react-hooks-update-6eb9b-default-rtdb.firebaseio.com/ingredients.json' + query)
+                    .then(response => response.json())
+                    .then(responseData => {
+                        const loadedIngredients = [];
+                        for(const key in responseData) {
+                            loadedIngredients.push({
+                                id: key,
+                                title: responseData[key].title,
+                                amount: responseData[key].amount
+                            })
+                        }
+                        //we had a warning of including the props as a dependency because here we are relying on props
+                        //when destructuring since this is a dependency too, we can add that in the deps of useEffect and invoke without props keyword
+                        onLoadIngredients(loadedIngredients);
+                        //..
+                    });
+            }
+        }, 500)
+    //    this inputsEARCHrEF is also a dependency because we need to know if that has changed in order to execute the query into firebase or not
+    }, [enteredFilter, onLoadIngredients, inputSearchRef])
 
     return (
         <section className="search">
@@ -36,6 +46,7 @@ const Search = React.memo(props => {
                 <div className="search-input">
                     <label>Filter by Title</label>
                     <input
+                        ref={inputSearchRef}
                         type="text"
                         value={enteredFilter}
                         onChange={event => setEnteredFilter(event.target.value)}
